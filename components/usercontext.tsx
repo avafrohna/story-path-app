07bb4642f597037cdb706943d924329ab3159d3e
Ava Frohna <122284870+avafrohna@users.noreply.github.com>
2024-11-01 19:55:23 +1000

    final version user context

--- components/usercontext.tsx
+++ components/usercontext.tsx
@@ -1,15 +1,19 @@
 import React, { createContext, useState, useContext, ReactNode } from 'react';
+import { UserContext } from '@/types/types';
 
-type UserContextType = {
-  username: string | null;
-  setUsername: (username: string) => void;
-  profilePicture: string | null;
-  setProfilePicture: (uri: string) => void;
-};
+// Create a context for managing user information.
+const UserContext = createContext<UserContext | undefined>(undefined);
 
-const UserContext = createContext<UserContextType | undefined>(undefined);
-
+/**
+ * UserProvider component that provides user context to its children.
+ * Manages and supplies username and profile picture information across the app.
+ * 
+ * @param {Object} props - Component props.
+ * @param {ReactNode} props.children - Components that require access to user data.
+ * @returns {JSX.Element} Context provider for user information.
+ */
 export const UserProvider = ({ children }: { children: ReactNode }) => {
+  // State to manage the username and profile picture
   const [username, setUsername] = useState<string | null>(null); 
   const [profilePicture, setProfilePicture] = useState<string | null>(null);
 
@@ -20,7 +24,14 @@ export const UserProvider = ({ children }: { children: ReactNode }) => {
   );
 };
 
-export const useUser = (): UserContextType => {
+/**
+ * Custom hook to access user context.
+ * Throws an error if used outside of a UserProvider.
+ * 
+ * @returns {UserContextType} The context value with user data and setters.
+ * @throws Will throw an error if `useUser` is not wrapped within a `UserProvider`.
+ */
+export const useUser = (): UserContext => {
   const context = useContext(UserContext);
   if (!context) {
     throw new Error('useUser must be used within a UserProvider');
